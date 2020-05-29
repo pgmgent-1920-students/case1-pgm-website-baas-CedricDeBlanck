3cfb721167c7f94d4870c74ed6379e814cdd579e
CedricDeBlanck <cedridebl1@student.arteveldehs.be>
2020-05-29 17:43:36 +0200

    wip

--- test/js/app.js
+++ test/js/app.js
@@ -1,9 +1,8 @@
 (() => {  
-    const DOMAIN = 'https://pgmgent-1920-students.github.io/case1-pgm-website-baas-CedricDeBlank';
     
   
     const loadBlog = async () => {
-      const response = await fetch(`${DOMAIN}/data/cases/index.json`);
+      const response = await fetch(`https://raw.githubusercontent.com/pgmgent-1920-students/case1-pgm-website-baas-CedricDeBlanck/master/docs/data/team/index.json?token=ANGVHBR64GASJTCGAYZ7IPS63JS5S`);
       const jsonData = await response.json();
      console.log(jsonData);    
     }
